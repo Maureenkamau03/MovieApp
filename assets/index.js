db1aad37e57b503195888332c26b03e93dbb8365
Maureenkamau03 <mkaymaureen@gmail.com>
2022-08-31 19:23:26 +0300

    fetching data

--- assets/index.js
+++ assets/index.js
@@ -5,19 +5,23 @@ function getMovieData() {
 		.then((res) => res.json())
 		.then((data) => {
 			// console.log(data)
-
-			let image = document.getElementById("img");
-			image.scr = data.results[1].backdrop_path;
-			let desc = document.getElementById("overview");
-			desc.innerText = data.results[1].overview;
-			console.log(data.results[1].overview);
+			function displayMovieData() {
+				image2.scr = data.results[2].backdrop_path;
+				desc2.innerText = data.results[2].overview;
+				// console.log(data.results[1].overview);
+				title2.innerText = data.results[2].title;
+				// console.log(data.results[2].title);
+				releaseDate2.innerText = data.results[2].first_air_date;
+				console.log(data.results[2].vote_count);
+			}
+			displayMovieData();
 		});
-}
-function displayMovieData() {
-	let image = document.getElementById("images");
-	image.scr = data.results.backdrop_path;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+	desc2 = document.getElementById("overview2");
+	image2 = document.getElementById("img2");
+	title2 = document.getElementById("title2");
+	releaseDate2 = document.getElementById("release");
 	getMovieData();
 });
